13dadec422c0b052ffb6d90856324ddc549819dc
王更新 <wgx954418992@qq.com>
2020-12-09 22:44:52 +0800

    Fast, simple and practical php framework

--- src/rapidPHP/modules/database/sql/classier/Utils.php
+++ src/rapidPHP/modules/database/sql/classier/Utils.php
@@ -2,12 +2,6 @@
 
 namespace rapidPHP\modules\database\sql\classier;
 
-
-use Exception;
-use rapidPHP\modules\core\classier\Model;
-use rapidPHP\modules\reflection\classier\Classify;
-
-
 class Utils
 {
 
@@ -34,61 +28,6 @@ class Utils
     public function formatColumn($column, $joinString = '`')
     {
         return preg_replace('/(.*?)(\w+)/i', "$1{$joinString}$2{$joinString}", $column);
-    }
-
-    /**
-     * 获取表name
-     * @param $table
-     * @param string $joinString
-     * @return mixed
-     */
-    public function getTableName($table, $joinString = '`')
-    {
-        if (is_subclass_of($table, Model::class) && $table::NAME != null) return $this->formatColumn($table::NAME, $joinString);
-
-        try {
-            $classify = Classify::getInstance($table);
-
-            /** @var DocComment $docComment */
-            $docComment = $classify->getDocComment(DocComment::class);
-
-            $tableAnnotation = $docComment->getTableAnnotation();
-
-            if ($tableAnnotation) {
-                $result = $tableAnnotation->getValue();
-
-            } else {
-                $result = is_object($table) ? get_class($table) : (string)$table;
-            }
-
-            return $this->formatColumn($result, $joinString);
-        } catch (Exception $e) {
-            $result = is_object($table) ? get_class($table) : (string)$table;
-
-            return $this->formatColumn($result, $joinString);
-        }
-    }
-
-    /**
-     * 获取表字段 通过model
-     * @param $model
-     * @param string $joinString
-     * @return mixed|string
-     */
-    public function getTableColumnByModel($model, $joinString = '`')
-    {
-        try {
-            $classify = Classify::getInstance($model);
-
-            $properties = $classify->getPropertiesNames();
-
-            if (empty($properties)) return '*';
-
-            return self::formatColumn(join(',', $properties), $joinString);
-        } catch (Exception $e) {
-            return '*';
-        }
-
     }
 }
 
