c5a63c0bc4fca0ce4afef2735e3e0859f4d6d011
王更新 <wgx954418992@qq.com>
2021-08-13 14:46:10 +0800

    fix bug

--- src/rapidPHP/modules/configure/classier/Configurator.php
+++ src/rapidPHP/modules/configure/classier/Configurator.php
@@ -1,21 +1,22 @@
 <?php
 
-namespace rapidPHP\modules\config\classier;
+namespace rapidPHP\modules\configure\classier;
 
 use Exception;
 use rapidPHP\modules\application\config\ApplicationConfig;
+use rapidPHP\modules\common\classier\AR;
 use rapidPHP\modules\common\classier\File;
 use rapidPHP\modules\common\classier\Instances;
 use rapidPHP\modules\common\classier\Variable;
-use rapidPHP\modules\config\classier\loader\JsonLoader;
-use rapidPHP\modules\config\classier\loader\PHPLoader;
-use rapidPHP\modules\config\classier\loader\XmlLoader;
-use rapidPHP\modules\config\classier\loader\YamlLoader;
-use rapidPHP\modules\config\classier\reflection\DocComment;
+use rapidPHP\modules\configure\classier\loader\JsonLoader;
+use rapidPHP\modules\configure\classier\loader\PHPLoader;
+use rapidPHP\modules\configure\classier\loader\XmlLoader;
+use rapidPHP\modules\configure\classier\loader\YamlLoader;
+use rapidPHP\modules\configure\classier\reflection\DocComment;
 use rapidPHP\modules\reflection\classier\Classify;
 use function rapidPHP\AR;
 
-class PConfig
+class Configurator implements IConfigurator
 {
 
     /**
@@ -53,24 +54,32 @@ class PConfig
     public function __construct()
     {
         $this->config = ApplicationConfig::getDefaultConfig();
+
+        Variable::parseVarByArray($this->config);
     }
 
     /**
      * set paths
      * @param array $paths
+     * @return Configurator
      */
     public function setPaths(array $paths)
     {
         $this->paths = $paths;
+
+        return $this;
     }
 
     /**
      * 追加path
      * @param string $path
+     * @return Configurator
      */
-    public function appendPath(string $path)
+    public function addPath(string $path)
     {
         $this->paths[] = $path;
+
+        return $this;
     }
 
     /**
@@ -88,6 +97,43 @@ class PConfig
     public function getConfig(): array
     {
         return $this->config;
+    }
+
+
+    /**
+     * 获取config value 里面的值
+     * 支持 a.b.c.e
+     * @param $name
+     * @return array|mixed|null
+     */
+    public function getConfigValue($name)
+    {
+        return AR()->value($this->config, $name);
+    }
+
+    /**
+     * set object properties
+     * @param $object
+     * @throws Exception
+     */
+    public static function setProperties($object)
+    {
+        $classify = Classify::getInstance($object);
+
+        $properties = $classify->getProperties();
+
+        foreach ($properties as $property) {
+            /** @var DocComment $comment */
+            $comment = $property->getDocComment(DocComment::class);
+
+            $config = $comment->getConfigAnnotation();
+
+            if ($config != null) {
+                $value = Configurator::getInstance()->getConfigValue($config->getName());
+
+                $property->setValue($value, $object);
+            }
+        }
     }
 
     /**
@@ -110,12 +156,15 @@ class PConfig
     /**
      * 载入配置文件
      * @param bool $isAppend
+     * @return Configurator
      * @throws Exception
      */
     public function load(bool $isAppend = true)
     {
         if (!$isAppend) {
             $this->config = ApplicationConfig::getDefaultConfig();
+
+            Variable::parseVarByArray($this->config);
         }
 
         foreach ($this->paths as $path) {
@@ -135,44 +184,10 @@ class PConfig
 
                 Variable::parseVarByArray($config);
 
-                $this->config = array_merge($this->config, $config);
+                AR::getInstance()->merge($this->config, $config);
             }
         }
-    }
 
-    /**
-     * 获取config value 里面的值
-     * 支持 a.b.c.e
-     * @param $name
-     * @return array|mixed|null
-     */
-    public function value($name)
-    {
-        return AR()->value($this->config, $name);
-    }
-
-    /**
-     * set object properties
-     * @param $object
-     * @throws Exception
-     */
-    public static function object($object)
-    {
-        $classify = Classify::getInstance($object);
-
-        $properties = $classify->getProperties();
-
-        foreach ($properties as $property) {
-            /** @var DocComment $comment */
-            $comment = $property->getDocComment(DocComment::class);
-
-            $config = $comment->getConfigAnnotation();
-
-            if ($config != null) {
-                $value = PConfig::getInstance()->value($config->getName());
-
-                $property->setValue($value, $object);
-            }
-        }
+        return $this;
     }
 }
